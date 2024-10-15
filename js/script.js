2b157e3034086bf03adb5ea935c59cc11aff84e3
BenDover333 <2103022bergs@zuyd.nl>
2024-10-15 15:54:02 +0200

    Code toegevoegd

--- js/script.js
+++ js/script.js
@@ -6,11 +6,54 @@ d3.select(".sporterGegevens")
     .append("p-text")
     .text("test")
 
+// const data = d3.csv("data/sportData.csv")
+//     .row(function(d) {
+//         return {
+//             voornaam: d.Voornaam,
+//             achternaam: d.Achternaam,
+//             leeftijd: d.Leeftijd
+//         };
+//     })
+//     .get(function(rows) {
+//         console.log(rows);
+//     });
+
+// d3.csv("data/sportData.csv", function(d) {
+//     return {
+//         voornaam: d.Voornaam,
+//         achternaam: d.Achternaam,
+//         leeftijd: +d.Leeftijd
+//     };
+// }, function(data) {
+//     console.log(data);
+// });
+
+d3.csv("data/sportData.csv", function(data) {   
+    d3.select(".sporterGegevens")
+        .selectAll("p")
+        .data(data)
+        .enter()
+        .append("p")
+        .text(function(d) {
+            return d.voornaam
+        })
+});
+
+
 d3.csv("data/sportData.csv", function(data) {
     for (let i = 0; i < data.length; i++) {
-        const element = data[i];
-        console.log(element);
+        const sporter = data[i];
+        
+        let voornaam = sporter.Voornaam;
+        let achternaam = sporter.Achternaam;
+        let leeftijd = sporter.Leeftijd;
+
+        console.log(achternaam);
     }
+});
+
+d3.csv("data/sportData.csv", function(data) {
+    console.log(data[0].Achternaam);
 });
 
 
